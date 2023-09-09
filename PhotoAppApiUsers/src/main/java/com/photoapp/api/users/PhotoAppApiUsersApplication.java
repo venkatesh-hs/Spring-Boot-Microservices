a05ae5ee0425f484fb876e5a2c342e7708a648e6
Venkatesh HS <hsvenkatesh10@gmail.com>
2023-09-09 10:35:16 +0530

    hystrix circuit breaker - fallBack method

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
