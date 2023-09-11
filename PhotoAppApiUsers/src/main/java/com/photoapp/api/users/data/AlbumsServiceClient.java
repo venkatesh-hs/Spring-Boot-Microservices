f628bb28e2de901d3e303aaa8452e7304e31d1e4
Venkatesh HS <hsvenkatesh10@gmail.com>
2023-09-11 20:22:54 +0530

    resilience4j - Retry Module

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/data/AlbumsServiceClient.java
@@ -2,6 +2,7 @@ package com.photoapp.api.users.data;
 
 import com.photoapp.api.users.ui.model.AlbumResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.util.List;
 @FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
     @GetMapping("/users/{id}/albums")
+    @Retry(name = "albums-ws")
     @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
 
